6b95d747afa8d89329808ce9e274e47dc3cf15bf
Gapryong-Kim <jimcalstrom@gmail.com>
2025-10-23 12:55:28 +0100

    Update unique.py

--- utility/unique.py
+++ utility/unique.py
@@ -1,8 +1,10 @@
-message = input("Enter message: ").replace(" ", "")
-for block_length in range(1, 200):
-    blocked = ""
-    for index, i in enumerate(message):
-        if (index) % block_length == 0:
-            blocked += " "
-        blocked += i
-    print(f"{block_length}: {len(list(set(blocked.split())))}")
+def unique(message):
+    lengths=[]
+    for block_length in range(1, 100):
+        blocked = ""
+        for index, i in enumerate(message):
+            if (index) % block_length == 0:
+                blocked += " "
+            blocked += i
+        lengths.append(f"{block_length}: {len(list(set(blocked.split())))}")
+    return lengths
