95385f1d3d6ff82c80f2b7a8e57f314a62314d3e
Gapryong-Kim <jimcalstrom@gmail.com>
2025-10-25 07:36:24 +0100

    Delete utility directory

--- utility/pair.py
+++
@@ -1,8 +0,0 @@
-message = input("Enter a message: ").replace(" ", "")
-paired = ""
-for i, k in enumerate(message):
-    if (i) % 2 == 0:
-        paired += " "
-    paired += k
-
-print(paired)
--- utility/splice.py
+++
@@ -1,9 +0,0 @@
-message1 = input("Enter a message: ").replace(" ", "")
-message2 = input("Enter a message: ").replace(" ", "")
-messages = [message1, message2]
-decoded = ""
-for i in list(zip(*messages)):
-    for x in i:
-        decoded += x
-print()
-print(decoded)
