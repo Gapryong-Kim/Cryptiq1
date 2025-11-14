c45840fc2aab2cd69f5f70163a15d22838f8ddb6
Gapryong-Kim <jimcalstrom@gmail.com>
2025-11-14 20:00:04 +0000

    Update app.py

--- CryptiQ/app.py
+++ CryptiQ/app.py
@@ -299,7 +299,7 @@ def breaker():
             elif cipher_type == "substitution":
                 key, plaintext = substitution_break(
                     text,
-                    max_restarts=1, sa_steps=3200, seed=42,
+                    max_restarts=1, sa_steps=6000, seed=42,
                     time_limit_seconds=8, threads=None,
                     fixed=fixed_map, verbose=True
                 )
@@ -1474,3 +1474,4 @@ def admin_ban_user():
 # ------------------- Run -------------------
 if __name__ == "__main__":
     app.run(debug=True)
+
