776ee22eb039af8c6d52edbd45007ec7463ce966
Gapryong-Kim <jimcalstrom@gmail.com>
2025-11-15 09:11:05 +0000

    Add files via upload

--- CryptiQ/app.py
+++ CryptiQ/app.py
@@ -307,7 +307,16 @@ def breaker():
         fixed=fixed_map,
         verbose=True
     )
-                
+                new_key,plaintext=key, plaintext = substitution_break(
+        plaintext,
+        max_restarts=1,
+        sa_steps=2000,
+        seed=42,
+        time_limit_seconds=5,
+        threads=None,   # auto: Render → 1, local → cores
+        fixed=fixed_map,
+        verbose=True
+    )
 
 
 
