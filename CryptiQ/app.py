0558115cdfedbbf7da07ffcae926d9abaced7854
Gapryong-Kim <jimcalstrom@gmail.com>
2025-11-15 15:14:45 +0000

    Update app.py

--- CryptiQ/app.py
+++ CryptiQ/app.py
@@ -312,10 +312,10 @@ def breaker():
                 
                 key, plaintext = substitution_break(
                     text,
-                    max_restarts=3,
+                    max_restarts=4,
                     sa_steps=3000,
                     seed=42,
-                    time_limit_seconds=11,
+                    time_limit_seconds=14,
                     threads=1,   # auto: Render → 1, local → cores
                     fixed=fixed_map,
                     verbose=True
@@ -1494,4 +1494,5 @@ def admin_ban_user():
 # ------------------- Run -------------------
 if __name__ == "__main__":
     app.run(debug=True)
+
 
